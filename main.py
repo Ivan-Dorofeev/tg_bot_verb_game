91e83d8937194e7b3982301a713779fd12da21d0
Ivan Dorofeev <axxel123@rambler.ru>
2023-02-25 09:43:14 +0300

    start bot

--- main.py
+++ main.py
@@ -1,78 +1,50 @@
+import asyncio
+import logging
 import os
 
-from google.cloud import dialogflow
+import telegram
 from dotenv import load_dotenv
-from telegram import ForceReply, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
-from telegram import __version__ as TG_VER
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext, Application, ContextTypes
 
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    print(__version_info__)
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
-
-
-def detect_intent_texts(project_id, session_id, texts, language_code):
-    credentials = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
-
-    session_client = dialogflow.SessionsClient(credentials=credentials)
-    session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
-
-    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+        rf"Здравствуй, {user.mention_html()}!",
+        # reply_markup=ForceReply(selective=True),
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Сам ты Help!")
+    await update.message.reply_text("Сам себе Help!")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    tg_chat_id = os.environ['TG_MY_CHAT_ID']
-    message = update.message.text
-    print(message)
-
-    detect_intent_texts(project_id='axxel123-gtnf', session_id=tg_chat_id, texts=message, language_code='ru-RU')
     await update.message.reply_text(update.message.text)
 
 
 def main():
     load_dotenv()
+    bot_token = os.environ['TG_BOT_TOKEN']
+    my_chat_id = os.environ['TG_MY_CHAT_ID']
 
-    tg_token = os.environ['TG_BOT_TOKEN']
+    application = Application.builder().token(bot_token).build()
 
-    application = ApplicationBuilder().token(tg_token).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
     application.run_polling()
 
 
